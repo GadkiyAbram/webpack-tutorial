c9a1bbe5db40396cc7f73b3498a30a31a6c5e48d
gadkiyabram <a.s.abramovskiy@tmn3.etagi.com>
2022-08-22 21:15:11 +0500

    separated to common / dev / prod configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,30 @@ const common = require('./webpack.common');
 const {merge} = require('webpack-merge');
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
     mode: "production",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.[contenthash].[bundle].js',
+        filename: '[name].[contenthash].[bundle].js',
     },
-    plugins: [new CleanWebpackPlugin()]
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, //Extract css into files
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+        ]
+    }
 });
